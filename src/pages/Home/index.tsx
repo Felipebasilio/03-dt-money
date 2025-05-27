177248329d9a7fa7d51dbf0a7d4c3f4930093c28
Felipe Basilio <fcb.97@hotmail.com>
2025-05-27 01:24:30 -0300

    Added forms to the project using validation schema (zod) and react hook form to deal properly with form data and its manipulation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,41 @@
 import styles from "./styles.module.css";
 import { StartCountdownButton } from "@/components";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+// A form can be controlled or uncontrolled.
+// A controlled form is a form that is controlled by the state of the component.
+// An uncontrolled form is a form that is controlled by the DOM.
+
+const newCycleFormValidationSchema = z.object({
+  task: z.string().min(1, "Informe a tarefa"),
+  minutesAmount: z
+    .number()
+    .min(5, "O tempo mínimo deve ser de 5 minutos")
+    .max(60, "O tempo máximo deve ser de 60 minutos"),
+});
 
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<z.infer<typeof newCycleFormValidationSchema>>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
+  function handleCreateNewCycle(data: z.infer<typeof newCycleFormValidationSchema>) {
+    console.log(data);
+    reset();
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <div className={styles.homeContainer}>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
         <div className={styles.formContainer}>
           <label htmlFor="task">Vou trabalhar em</label>
           <input
@@ -12,6 +43,7 @@ export function Home() {
             id="task"
             placeholder="Dê um nome para o seu projeto"
             className={styles.taskInput}
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -23,7 +55,16 @@ export function Home() {
           </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" placeholder="00" className={styles.minutesAmountInput} step={5} min={5} max={60} />
+          <input
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            className={styles.minutesAmountInput}
+            step={5}
+            min={5}
+            max={60}
+            {...register("minutesAmount", { valueAsNumber: true })}
+          />
 
           <span>minutos.</span>
         </div>
@@ -36,7 +77,7 @@ export function Home() {
           <span>0</span>
         </div>
 
-        <StartCountdownButton />
+        <StartCountdownButton disabled={isSubmitDisabled} />
       </form>
     </div>
   );
